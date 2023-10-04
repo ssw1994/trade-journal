5f476b66d1d7bff0d3c02c35b7dff5c3993296dd
esachin <waghmare.sachin1994@gmail.com>
2023-10-04 10:59:19 +0530

    seventh deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
